64c9a9575c619c1adb845b665dac29e82ce4ddd6
Hectormp4 <165405547+Hectormp4@users.noreply.github.com>
2025-10-14 18:31:31 -0500

    Update vital_indstural_project.ino

--- vital_indstural_project/vital_indstural_project.ino
+++ vital_indstural_project/vital_indstural_project.ino
@@ -1,21 +1,42 @@
-#include <MAX30100.h>
 #include <Wire.h>
 #include "Protocentral_MAX30205.h"
+#include "MAX30100_PulseOximeter.h"
+#define REPORTING_PERIOD_MS 1000
+const int GSRpin = A0;
 
-const int GSRpin = A0; 
 MAX30205 tempSensor; 
+PulseOximeter pox;
+
+uint32_t tsLastReport = 0;
+
+void onBeatDetected(){
+  Serial.println("Beat!!");
+}
 
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
   Wire.begin();
+  Serial.begin(9600);
+  Serial.begin(115200);  
+
+  //MAX30100 scans 
+  Serial.println("Initializing pulse oximeter..");
+  if (!pox.begin()){
+    Serial.println("FAILED");
+    for(;;);
+    }else{
+      Serial.println("SUCCESS");
+    }
+    pox.setOnBeatDetectedCallback(onBeatDetection)
+  }
 
   //MAX30205 scans
   while(!tempSensor.scanAvailableSensors()){
     Serial.println("No sensor input was found, please retry.");
     delay(30000); //scans every 30 seconds
   }
+
   tempSensor.begin(); //makes the scanning constant 
 }
 
@@ -31,6 +52,6 @@ void loop() {
   int sensorValue = analogRead(GSRpin);
   float voltage = sensorValue * (5/1023);
   Serial.print(voltage);
-  Serial.println(" Voltage value");
+  Serial.println(" : Voltage value");
   delay(500);
  }
